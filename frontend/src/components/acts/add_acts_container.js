c255545ec0d9c90afef18cff85a569440dc47e1c
k-2tha-brimm <kev.brimm@gmail.com>
2019-06-03 22:03:03 -0700

    steal andrews mapping from dashboard, but still struggling with getting component to remain mounted

--- frontend/src/components/acts/add_acts_container.js
+++ frontend/src/components/acts/add_acts_container.js
@@ -4,13 +4,16 @@ import { fetchGroup, updateGroup } from '../../actions/group_actions';
 import { fetchAct } from '../../actions/act_actions';
 import { fetchUserGroups } from '../../actions/user_actions';
 import { closeModal } from '../../actions/modal_actions';
+import Loading from '../loading/loading';
 import '../../assets/stylesheets/modal.css';
 import '../../assets/stylesheets/acts_show.css'
 
 class AddActsForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            loading: true
+        }
     }
 
     componentDidMount() {
@@ -22,59 +25,79 @@ class AddActsForm extends React.Component {
                             this.setState({ [groupId]: res.group.data })
                         })
                 ))
+                this.setState({ loading: false });
             });
-        // this.setState({ act: this.props.act });
     }
 
-    componentWillUnmount() {
-        this.props.closeModal();
-    }
+    // componentWillUnmount() {
+    //     this.props.closeModal();
+    // }
 
-    updateActs() {
-        return (e) => this.setState({
+    // update(field) {
+    //     return (e) => this.setState({
+    //         [field]: field.push(this.state.act)
+    //     });
+    // }
 
-        });
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-    }
+    // handleSubmit(e) {
+    //     e.preventDefault();
+    // }
 
     render() {
 
-        if(!this.state.groups) {
-            return null;
-        }
+        if (this.state.loading) {
+            return <Loading />
+        };
 
-        let groups = this.state.groups.map((groupId) => {
-            return this.state[groupId].name;
-        });
+        if (this.state.groups) {
+            this.state.groups.map((group_id) => {
+              if (group_id !== 'loading' && group_id !=='activePanel') {
+                return (
+                <div className="session-form-modal">
+                    <h1 className="delete-header">Add This Act</h1>
+
+                    <h3>Choose a group to add this act to below:</h3>
+                    <li 
+                        key={group_id} 
+                        className={`group-index-item ${group_id}`}
+                        onClick={this.handleDisplay}
+                    > 
+                        {this.state[group_id].name}
+                    </li>
+                </div>
+                )
+              }
+              return null;
+            });
+          } else {
+              return <Loading />;
+          }
+
     
-        return (
-            <div className="add-act-modal">
+        // return (
+        //     <div className="session-form-modal">
 
-                <h1 className="delete-header">Add This Act</h1>
+                // <h1 className="delete-header">Add This Act</h1>
 
-                <h3>Choose a group to add this act to below:</h3>
+                // <h3>Choose a group to add this act to below:</h3>
 
-                <li>{groups}</li>
-                {/* <select>
-                </select> */}
+        //         <select>
+        //         </select>
 
-            </div>
-        )
+        //         <button className="create-button">Add Act</button>
+        //     </div>
+        // )
     }
 }
 
-const mstp = (state, ownProps) => {
+const mstp = state => {
     return {
         act: state.act,
-        currentUser: state.session.user,
-        formType: 'Add Act'
+        currentUser: state.session.user
     };
 };
 
-const mdtp = (dispatch) => {
+const mdtp = dispatch => {
     return {
         fetchAct: id => dispatch(fetchAct(id)),
         upateGroup: group => dispatch(updateGroup(group)),
