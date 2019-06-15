129471f92ea01f40deded5c126ae091e07986e8c
k-2tha-brimm <kev.brimm@gmail.com>
2019-06-15 14:11:24 -0700

    export server, add tests for getting demo users groups and posting groups

--- test/group.js
+++ test/group.js
@@ -9,12 +9,50 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('Groups', () => {
-    beforeEach(done => {
-        
-    })
-    describe('/GET group', ()=> {
-        it('it should get all a users groups', done => {
+    describe('/GET group', () => {
+        it('it should get all of a users groups', done => {
+            chai.request(server)
+                .get(`/api/users/5cdafb8c9f82fb344e8a45df/groups`)
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a('array');
+                done();
+                });
+        });
+    });
 
+    describe('/POST group', () => {
+        it('it should not create a group with no name', done => {
+            let group = {
+                name: ''
+            }
+            chai.request(server)
+                .post('/api/groups')
+                .send(group)
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a('object');
+                    response.body.should.have.property('errors');
+                    response.body.errors.should.have.property('name');
+                    response.body.errors.pages.should.have.property('kind').eql('required');
+                done();
+                });
+        });
+        it('should create a group with proper inputs', done => {
+            let group = {
+                name: 'Chad n the Bros'
+            }
+            chai.request(server)
+                .post('/api/groups')
+                .send(group)
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a('object');
+                    response.body.group.should.have.property('owner');
+                    response.body.group.should.have.property('acts');
+                    response.body.group.should.have.property('members');
+                done();
+                })
         })
-    })
-})
+    });
+});
