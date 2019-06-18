fa338c52a0870259dc908440cd506b7196de5765
k-2tha-brimm <kev.brimm@gmail.com>
2019-06-17 19:21:10 -0700

    fixed issue with auth tokens. sometimes tests timeout.

--- test/group.js
+++ test/group.js
@@ -5,7 +5,7 @@ const server = require('../app');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const should = chai.should();
-const request = require('superagent');
+const request = require('supertest')(server);
 
 chai.use(chaiHttp);
 
@@ -13,41 +13,60 @@ describe('Groups', () => {
 
     describe('User login', () => {
         it('should return 200 and token for valid credentials', done => {
+            setTimeout(done(), 30000);
             const user = {
-                "username": "demo_user",
+                "username": "kevin",
                 "password": "password"
             }
 
-            chai.request(server)
+            request
                 .post(`/api/users/login`)
                 .send(user)
                 .then(res => {
                     res.should.have.status(200);
                     res.body.should.have.property('token');
-                done();
-                })
-        })
-    })
-    
-    describe('/GET group', () => {
-        it('it should get all of a users groups', done => {
-            chai.request(server)
-                .get(`/api/users/5cdafb8c9f82fb344e8a45df/groups`)
-                .end((err, response) => {
-                    response.should.have.status(200);
-                    response.body.should.be.a('array');
-                done();
                 });
         });
     });
 
-    describe('/POST group', () => {
+    describe('GET group', () => {
+        let auth = {};
+        beforeEach(loginUser(auth));
+
+        it('should resopnd with JSON array', done => {
+            setTimeout(done(), 30000);
+            request
+                .get(`/api/users/5cdafba39f82fb344e8a45e0/groups`)
+                .set('Authorization', 'Bearer ' + auth.token)
+                .end((err, response) => {
+                    if(err) return done(err);
+                });
+        });
+
+        it('it should get all of a users groups', done => {
+            setTimeout(done(), 30000);
+            request
+                .get(`/api/users/5cdafba39f82fb344e8a45e0/groups`)
+                .set('Authorization', 'Bearer ' + auth.token)
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a('array');
+                });
+        });
+    });
+
+    describe('POST group', () => {
+        let auth = {};
+        before(loginUser(auth));
+
         it('it should not create a group with no name', done => {
+            setTimeout(done(), 30000);
             let group = {
                 name: ''
             }
-            chai.request(server)
+            request
                 .post('/api/groups')
+                .set('Authorization', 'Bearer ' + auth.token)
                 .send(group)
                 .end((err, response) => {
                     response.should.have.status(200);
@@ -55,15 +74,17 @@ describe('Groups', () => {
                     response.body.should.have.property('errors');
                     response.body.errors.should.have.property('name');
                     response.body.errors.pages.should.have.property('kind').eql('required');
-                done();
                 });
         });
+
         it('should create a group with proper inputs', done => {
+            setTimeout(done(), 30000);
             let group = {
                 name: 'Chad n the Bros'
             }
-            chai.request(server)
+            request
                 .post('/api/groups')
+                .set('Authorization', 'Bearer ' + auth.token)
                 .send(group)
                 .end((err, response) => {
                     response.should.have.status(200);
@@ -71,8 +92,25 @@ describe('Groups', () => {
                     response.body.group.should.have.property('owner');
                     response.body.group.should.have.property('acts');
                     response.body.group.should.have.property('members');
-                done();
-                })
-        })
+                });
+        });
     });
 });
+
+function loginUser(auth) {
+    return function(done) {
+        request
+            .post(`/api/users/login`)
+            .send({
+                username: 'kevin',
+                password: 'password'
+            })
+            .expect(200)
+            .end(onResponse);
+
+            function onResponse(err, res) {
+                auth.token = res.body.token;
+                return done();
+            };
+    };
+};
